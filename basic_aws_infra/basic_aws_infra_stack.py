7ec30b4daee8a236cc42f98d2c401927f14b8807
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-11-19 08:14:35 +0000

    First commit

--- basic_aws_infra/basic_aws_infra_stack.py
+++ basic_aws_infra/basic_aws_infra_stack.py
@@ -1,12 +1,19 @@
 from aws_cdk import (
+    RemovalPolicy,
+    # aws_sqs as sqs,
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+)
+from aws_cdk import (
+    aws_ec2 as ec2,
+)
+from aws_cdk import (
+    aws_s3 as s3,
 )
 from constructs import Construct
 
-class BasicAwsInfraStack(Stack):
 
+class BasicAwsInfraStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
@@ -17,3 +24,26 @@ class BasicAwsInfraStack(Stack):
         #     self, "BasicAwsInfraQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+        vpc = ec2.Vpc(
+            self,
+            "FirstVpc",
+            max_azs=2,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="PublicSubnet", subnet_type=ec2.SubnetType.PUBLIC, cidr_mask=24
+                )
+            ],
+        )
+
+        instance = ec2.Instance(
+            self,
+            "MyInstance",
+            instance_type=ec2.InstanceType("t2.micro"),
+            machine_image=ec2.MachineImage.latest_amazon_linux(),
+            vpc=vpc,
+            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+        )
+
+        bucket = s3.Bucket(
+            self, "DataBucket", versioned=True, removal_policy=RemovalPolicy.DESTROY
+        )
